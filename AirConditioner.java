dc7bb3850de32f6b1f9b598e3b9ccbe47fddbba5
moscarce <oluwa14tobi@gmail.com>
2023-05-28 20:17:39 +0100

    air conditioner assignment

---
+++ AirConditioner.java
@@ -0,0 +1,34 @@
+public class AirConditioner {
+		private boolean isOn;
+		private String productName;
+		private int temperature;
+		
+		public String AirConditioner(){
+			return AirConditioner();
+		}
+		public void decreaseTemperature(){
+		
+		}
+		public String getProductName(){
+			return productName;
+		}
+		public int getTemperature(){
+			return temperature;
+		}
+		public void increaseTemprature(){
+		
+		}
+		public boolean isOn(){
+			return isOn;
+		}
+		public void setOn(boolean isOn){
+		
+		}
+		public void setProductName(String productName){
+		
+		}
+		public void setTemperature(int temperature){
+		
+		}
+	
+	}
